117101c340819386a8a80e5eceb5e527cab48e7e
vladimirmelnic <vladimir.melnic96@gmail.com>
2021-10-23 20:53:47 +0300

    Introduced classes and settings.json file. Fully working version of the bot. Note: care of spam-ban from telegram.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import http.client
 import os
 import time
 import json
@@ -8,150 +9,230 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
 
-MAIN_URL = 'https://999.md/ru/list/transport/cars?view_type=short'
-TEMP_FILENAME = 'tmp_file.html'
-MAIN_FILE = 'all_info.json'
-BOT_DATA = 'bot_data.json'
+class Parser:
 
+    def __init__(self):
 
-def save_link_data(link: str):
-
-    headers = {
-        "Accept": "*/*",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
-    }
-
-    html = requests.get(link, headers=headers).text
-
-    with open(TEMP_FILENAME, "w") as file:
-        file.write(html)
-
-
-def read_link_data() -> BeautifulSoup:
-
-    with open(TEMP_FILENAME) as file:
-        html = file.read()
-
-    # Deleting temp file, because it no more needed
-    os.remove(TEMP_FILENAME)
-
-    return BeautifulSoup(html, "lxml")
-
-
-def get_data(link_data: BeautifulSoup):
-
-    info = []
-
-    table = link_data.find('table', class_='ads-list-table')
-    rows = table.find_all('tr')
-
-    for tr in rows:
-
-        # Link and Title
-        link_title = tr.find('td', class_='ads-list-table-title').find('a')
-        link = 'https://999.md' + link_title.get('href')
-
-        if link.find('booster') == -1:
-            title = link_title.text.strip()
-            year = tr.find('td', class_='ads-list-table-col-3 feature-19').text.strip()
-            engine = tr.find('td', class_='ads-list-table-col-4 feature-103').text.strip()
-            mileage = tr.find('td', class_='ads-list-table-col-4 feature-104').text.strip()
-            transmission = tr.find('td', class_='ads-list-table-col-2 feature-101').text.strip()
-            # Price could be 'договорная'
-            try:
-                price = tr.find('td', class_='ads-list-table-price feature-2').text.strip()
-            except Exception:
-                price = tr.find('td', class_='ads-list-table-price feature-2 is-negotiable').text.strip()
-            date = tr.find('td', class_='ads-list-table-date').text.strip()
-            try:
-                image_link = tr.find('div', class_='photo js-tooltip-photo').get('data-image')
-                image_link = image_link.split('?')[0]
-            except Exception:
-                image_link = 'No photo'
-
-            info.append(
-                {'Link': link,
-                 'Title': title,
-                 'Year': year,
-                 'Engine': engine.replace('    ', ' '),
-                 'Mileage': mileage.replace('    ', ' '),
-                 'Transmission': transmission,
-                 'Price': price.replace('\u00A0', ' '),
-                 'Date': date,
-                 'ImageLink': image_link}
-            )
-
-    return info
-
-
-def parsing():
-
-    save_link_data(MAIN_URL)
-
-    # Current INFO
-    if os.stat(MAIN_FILE).st_size > 0:
-        with open(MAIN_FILE, 'r') as file:
-            current_info = json.load(file)
-            last_link = current_info[len(current_info) - 1].get('Link')
+        # Reading file and getting settings
+        with open('settings.json', 'r') as file:
+            file_data = json.load(file)
+            self.main_url = file_data.get('main_url')
+            self.temp_filename = file_data.get('temp_filename')
+            self.main_filename = file_data.get('main_filename')
+            self.bot_data_filename = file_data.get('bot_data_filename')
             file.close()
-    else:
-        last_link = None
-        current_info = []
 
-    # Parsing
-    link_data = read_link_data()
-    data = get_data(link_data)
-    data.reverse()
-    last_link_found = False or last_link is None
-    for info in data:
-        current_link = info.get('Link')
-        if current_link == last_link:
-            last_link_found = True
-        elif last_link_found:
-            current_info.append(info)
+        # Checking that service file exists
+        if not os.path.isfile(self.main_filename):
+            with open(self.main_filename, 'x') as file:
+                pass
 
-    # Adding info into file
-    with open(MAIN_FILE, 'w') as file:
-        json.dump(current_info, file, indent=4, ensure_ascii=False)
+        print('Parser initialized!')
+
+    def save_link_data(self, link: str):
+
+        headers = {
+            "Accept": "*/*",
+            "User-Agent": "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16.2"
+        }
+
+        session = requests.Session()
+        html = session.get(url=link, headers=headers).text
+
+        with open(self.temp_filename, "w") as file:
+            file.write(html)
+
+    def read_link_data(self) -> BeautifulSoup:
+
+        with open(self.temp_filename, 'r') as file:
+            html = file.read()
+            file.close()
+
+        # Deleting temp file, because it no more needed
+        os.remove(self.temp_filename)
+
+        return BeautifulSoup(html, "lxml")
+
+    def get_data(self, link_data: BeautifulSoup) -> list:
+
+        info = []
+
+        table = link_data.find('table', class_='ads-list-table')
+        rows = table.find_all('tr')
+
+        for tr in rows:
+
+            # Link and Title
+            link_title = tr.find('td', class_='ads-list-table-title').find('a')
+            link = 'https://999.md' + link_title.get('href')
+
+            if link.find('booster') == -1:
+                title = link_title.text.strip()
+                year = tr.find('td', class_='ads-list-table-col-3 feature-19').text.strip()
+                engine = tr.find('td', class_='ads-list-table-col-4 feature-103').text.strip()
+                mileage = tr.find('td', class_='ads-list-table-col-4 feature-104').text.strip()
+                transmission = tr.find('td', class_='ads-list-table-col-2 feature-101').text.strip()
+                # Price could be 'договорная'
+                try:
+                    price = tr.find('td', class_='ads-list-table-price feature-2').text.strip()
+                except Exception:
+                    price = tr.find('td', class_='ads-list-table-price feature-2 is-negotiable').text.strip()
+                date = tr.find('td', class_='ads-list-table-date').text.strip()
+                try:
+                    image_link = tr.find('div', class_='photo js-tooltip-photo').get('data-image')
+                    image_link = image_link.split('?')[0]
+                except Exception:
+                    image_link = 'No photo'
+
+                info.append(
+                    {'Link': link,
+                     'Title': title,
+                     'Year': year,
+                     'Engine': engine.replace('    ', ' '),
+                     'Mileage': mileage.replace('    ', ' '),
+                     'Transmission': transmission,
+                     'Price': price.replace('\u00A0', ' '),
+                     'Date': date,
+                     'ImageLink': image_link}
+                )
+
+        return info
+
+    def parsing(self) -> list:
+
+        new_info = []
+
+        self.save_link_data(self.main_url)
+
+        # Current INFO
+        if os.stat(self.main_filename).st_size > 0:
+            with open(self.main_filename, 'r') as file:
+                current_info = json.load(file)
+                last_link = current_info[len(current_info) - 1].get('Link')
+                file.close()
+        else:
+            last_link = None
+            current_info = []
+
+        # Parsing
+        link_data = self.read_link_data()
+        data = self.get_data(link_data)
+        data.reverse()
+        last_link_found = False or last_link is None
+        for info in data:
+            current_link = info.get('Link')
+            if current_link == last_link:
+                last_link_found = True
+            elif last_link_found:
+                current_info.append(info)
+                new_info.append(info)
+
+        # Adding info into file
+        with open(self.main_filename, 'w') as file:
+            json.dump(current_info, file, indent=4, ensure_ascii=False)
+
+        return new_info
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I can send you all new ads of cars from 999.md")
+class TelegramBot:
 
+    def __init__(self):
 
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+        # Reading file and getting settings
+        with open('settings.json', 'r') as file:
+            file_data = json.load(file)
+            self.bot_data_filename = file_data.get('bot_data_filename')
+            self.bot_token = file_data.get('bot_token')
+            file.close()
+
+        # Initializing current user info
+        if not os.path.isfile(self.bot_data_filename):
+            with open(self.bot_data_filename, 'x') as file:
+                self.current_ids = []
+        else:
+            with open(self.bot_data_filename, 'r') as file:
+                self.current_ids = json.load(file)
+                file.close()
+
+        # Main telegram UPDATER
+        self.updater = Updater(token=self.bot_token, use_context=True)
+        self.dispatcher = self.updater.dispatcher
+
+        # Handlers
+        self.dispatcher.add_handler(CommandHandler('start', self.start))
+        self.dispatcher.add_handler(CommandHandler('stop', self.stop))
+        self.dispatcher.add_handler(MessageHandler(Filters.command, self.unknown))
+
+        # Starting the bot
+        self.updater.start_polling()
+
+        print('Telegram bot initialized!')
+
+    def add_user(self, user_data: dict):
+
+        key_found = False
+        for item in self.current_ids:
+            if item.get('user_id') == user_data.get('user_id'):
+                key_found = True
+                break
+        if not key_found:
+            self.current_ids.append(user_data)
+            # Updating user info file
+            with open(self.bot_data_filename, 'w') as file:
+                json.dump(self.current_ids, file, indent=4, ensure_ascii=False)
+
+    def remove_user(self, user_id: str):
+
+        for i in range(self.current_ids.count()):
+            if self.current_ids[i].get('user_id') == user_id:
+                self.current_ids.remove(i)
+                break
+
+        # Updating user info file
+        with open(self.bot_data_filename, 'w') as file:
+            json.dump(self.current_ids, file, indent=4, ensure_ascii=False)
+
+    def start(self, update, context):
+
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="Hi! I will send you all new ads of the cars from 999.md")
+
+        # Saving user ID
+        for key in context.dispatcher.chat_data.keys():
+            user_data = {"user_id": key,
+                         "filters:": "bmw, mercedes"}
+            self.add_user(user_data)
+
+    def stop(self, update, context):
+
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='To start receiving new adds - write /start and choose filters')
+
+        self.remove_user(update.effective_chat.id)
+
+    def unknown(self, update, context):
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 
 def main():
 
-    # Checking that service files exists
-    if not os.path.isfile(MAIN_FILE):
-        with open(MAIN_FILE, 'x') as file:
-            pass
-    if not os.path.isfile(BOT_DATA):
-        with open(BOT_DATA, 'x') as file:
-            pass
-
-    # Telegram bot service code
-    updater = Updater(token='1915826120:AAELKWL6xfLbgQT7l0FRmwzYtGLwzOjXkaQ', use_context=True)
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    dispatcher = updater.dispatcher
 
-    # Commands
-    # START
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-    # UNKNOWN COMMAND
-    unknown_handler = MessageHandler(Filters.command, unknown)
-    dispatcher.add_handler(unknown_handler)
+    parser = Parser()
 
-    # Starting the bot
-    updater.start_polling()
+    telegram_bot = TelegramBot()
 
-    for i in range(2):
-        time.sleep(2)
-        parsing()
+    while True:
+        new_info = parser.parsing()
+        for info in new_info:
+            for user_info in telegram_bot.current_ids:
+                chat_id = user_info.get('user_id')
+                title = info.get('Title')
+                telegram_bot.updater.bot.send_message(chat_id=chat_id, text=f'<b>{title}</b>', parse_mode='HTML')
+            time.sleep(1)
+
+    telegram_bot.updater.idle()
 
 
 if __name__ == '__main__':
